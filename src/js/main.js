402e741243ce1a0ab76f90858471c2f1cdcae613
sali1502 <sali1502@sudent.miun.se>
2024-11-20 17:14:20 +0100

    Grundstruktur med vanlig CSS och JS på första och sista sidan.

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+/* DT193G Moment 1 - CSS-ramverk, Åsa Lindskog sali102@student.miun.se
+*/ 
+
+"use strict";
+
+/* Meny */
+
+// Hämta meny-knappar
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+
+// Eventlyssnare
+openBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+
+// Toggla fram navigeringsmeny
+function toggleMenu() {
+    let navMenuEl = document.getElementById("nav-menu");
+
+    // Hämta in css för meny
+    let style = window.getComputedStyle(navMenuEl);
+
+    // Koll om navigering är synlig, ändrar display block/none
+    if (style.display === "none") {
+        navMenuEl.style.display = "block";
+    } else {
+        navMenuEl.style.display = "none";
+    }
+}
